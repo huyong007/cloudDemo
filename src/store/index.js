6e923f20a7040109ec91215a27e1a3a299526678
huyong007 <1341696804@qq.com>
2019-12-06 17:16:58 +0800

    vue.js快跑over

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,9 @@
-const store = {
-    debug: true,
-    state: {
-      message: 'Hello!',
-      number:-10
-    },
-    setMessageAction (newValue) {
-      // eslint-disable-next-line no-console
-      if (this.debug) console.log('setMessageAction triggered with', newValue)
-      this.state.number = newValue
-    },
-    clearMessageAction () {
-      // eslint-disable-next-line no-console
-      if (this.debug) console.log('clearMessageAction triggered')
-      this.state.message = ''
-    }
-  }
-  export default store;
+import Vue from "vue";
+
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {}
+});
